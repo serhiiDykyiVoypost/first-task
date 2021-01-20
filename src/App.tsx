703348193aa98a6ca55aeb0cfedd89675d676f73
Serhii Dykyi <serhii.dykyi@voypost.com>
2021-01-20 10:43:06 +0200

    add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-// import { ThemeProvider } from '@material-ui/core/styles';
-// import theme from './components/common/theme';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './components/common/theme';
 import Button from './components/Unknown/Button';
 import './App.css';
 
 function App() {
   return (
-    // <ThemeProvider theme={theme}>
-    <div className="App">
-      <Button color="primary" size="large" onClick={() => null}>
-        Sing Up With Google
-      </Button>
-    </div>
-    // </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Button color="primary" size="large" onClick={() => null}>
+          Sing Up With Google
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
